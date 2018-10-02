b4a38f4dc0aca92087ef3d178409d8641a2ede58
Abhinav Kr. Jha <abhinavkr.jha.3@gmail.com>
2018-10-03 00:23:44 +0530

    057

---
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+
+import fetchSongDetail from "../queries/fetchSongDetail";
+
+class SongDetail extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h3>Song Details</h3>
+      </div>
+    );
+  }
+}
+
+export default graphql(fetchSongDetail, {
+  options: props => {
+    return { variables: { id: props.params.id } };
+  }
+})(SongDetail);
