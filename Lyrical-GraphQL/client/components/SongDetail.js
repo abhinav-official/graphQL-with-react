57b67e95c0b55f398a55868caff0c99b61868f61
Abhinav Kr. Jha <abhinavkr.jha.3@gmail.com>
2018-10-05 00:47:57 +0530

    LyricCreate

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
 
 import fetchSongDetail from "../queries/fetchSongDetail";
+import LyricCreate from "./LyricCreate";
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    const { song } = this.props.data;
+
+    if (!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song Details</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
